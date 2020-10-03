385d9270d408f060a35b7763da7a05d52adcb307
nzalmalla <medenimmalla@gmail.com>
2020-10-02 22:02:39 -0400

    restSECURITY

---
+++ src/main/java/Service/RestAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package Service;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RestAuthenticationEntryPoint  implements AuthenticationEntryPoint {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException{
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+
+    }
+}
